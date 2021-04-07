9d5be344ba900f360ec15653035a98df27d6fc6c
TOMOHIRO <hurandall@yahoo.co.jp>
2021-04-07 18:54:49 +0900

    商品一覧まで

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NavbarComponent } from './commom/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent 
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
