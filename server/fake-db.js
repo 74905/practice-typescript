8d97fd20b1a5f0f53ff76e6f006ee5dc6b57f80b
TOMOHIRO <hurandall@yahoo.co.jp>
2021-04-09 19:10:53 +0900

    nodeサーバー

---
+++ server/fake-db.js
@@ -0,0 +1,77 @@
+const Product = require('./model/product')
+
+class FakeDb {
+  constructor(){
+    this.products =[
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: '強い',
+        heading2: '想い',
+        heading3: 'ここに',
+        headingtext1: '文章、文章文章、文章文章、文章文章、文章',
+        headingtext2: '文章、文章文章、文章文章、文章文章、文章文章、文章',
+        headingtext3: '文章、文章文章、文章文章、文章文章、文章'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: '文章、文章文章、文章文章、文章文章、文章',
+        headingtext2: '文章、文章文章、文章文章、文章文章、文章文章、文章',
+        headingtext3: '文章、文章文章、文章文章、文章文章、文章'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: '文章、文章文章、文章文章、文章文章、文章',
+        headingtext2: '文章、文章文章、文章文章、文章文章、文章文章、文章',
+        headingtext3: '文章、文章文章、文章文章、文章文章、文章'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 499,
+        description: '',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: '文章、文章文章、文章文章、文章文章、文章',
+        headingtext2: '文章、文章文章、文章文章、文章文章、文章文章、文章',
+        headingtext3: '文章、文章文章、文章文章、文章文章、文章'
+      }
+    ]
+  }
+  async initDb(){
+    await this.cleanDb()
+    this.pushProductsToDb()
+  }
+  async cleanDb(){
+   await Product.deleteMany({})
+  }
+  pushProductsToDb(){
+    this.products.forEach(
+      (product) => {
+        const newProduct = new Product(product)
+        newProduct.save()
+      }
+    )
+  }
+
+  seeDb(){
+    this.pushProductsToDb()
+  }
+}
+
+module.exports = FakeDb
